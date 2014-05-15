fc1ab606030c645148bef65a558778d8b9ecd538
Michael McEniry (UAH) <mcenirm@uah.edu>
2014-05-15 11:13:24 -0500

    try to figure out webapp2 routes

--- helloworld.py
+++ helloworld.py
@@ -4,10 +4,12 @@ class MainPage(webapp2.RequestHandler):
 
     def get(self):
 
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write(str(self.request.GET))
-		self.response.write('\n')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(self.request.path)
+        self.response.write('\n')
+        self.response.write(str(self.request.GET))
+        self.response.write('\n')
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    webapp2.Route('/.*', handler=MainPage, name='main'),
 ], debug=True)
