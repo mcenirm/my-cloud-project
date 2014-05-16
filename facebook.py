0bfb7d8ab52f8f982245bd3d914840b5581390ff
Michael McEniry (UAH) <mcenirm@uah.edu>
2014-05-16 13:40:50 -0500

    actually fetch from Facebook

--- facebook.py
+++ facebook.py
@@ -11,17 +11,20 @@ class FacebookHandler(webapp2.RequestHandler):
     def get(self, id):
 
         feed_url = 'https://www.facebook.com/feeds/page.php?id=' + id + '&format=json'
-        feed_file = 'facebook.' + id + '.json'
+        feed_response = urllib2.urlopen(feed_url)
 
-        self.response.headers['Last-Modified'] = 'Wed, 14 May 2014 11:16:06 -0700'
+        for header_name in [
+            'Expires',
+            'Last-Modified',
+            'Pragma',
+            'Cache-Control',
+        ]:
+            self.response.headers[header_name] = feed_response.headers[header_name.lower()]
         self.response.headers['Content-Type'] = 'application/rss+xml'
-        self.response.headers['Pragma'] = 'no-cache'
-        self.response.headers['Cache-Control'] = 'private, no-cache, no-store, must-revalidate'
-        self.response.headers['Expires'] = 'Sat, 01 Jan 2000 00:00:00 GMT'
 
-        feed_in = json.load(open(feed_file))
-        # self.response.write(json.dumps(feed_in, sort_keys=True, indent=4, separators=(',', ': ')))
-        out_args = dict(
+        feed_in = json.load(feed_response)
+
+        feed_out_args = dict(
             title=u'',
             link=u'',
             description=u'',
@@ -33,12 +36,12 @@ class FacebookHandler(webapp2.RequestHandler):
             elif in_key == 'entries':
                 pass
             else:
-                out_args[in_key] = unicode(in_value)
+                feed_out_args[in_key] = unicode(in_value)
 
-        feed_out = ContentEncodedFeed(**out_args)
+        feed_out = ContentEncodedFeed(**feed_out_args)
 
         for entry in feed_in['entries']:
-            out_args = dict(
+            entry_out_args = dict(
                 title=u'',
                 link=u'',
                 description=u'',
@@ -46,24 +49,24 @@ class FacebookHandler(webapp2.RequestHandler):
             for in_key, in_value in entry.iteritems():
                 if in_key in ['author']:
                     for ak, av in in_value.iteritems():
-                        out_args[in_key + '_' + ak] = unicode(av)
+                        entry_out_args[in_key + '_' + ak] = unicode(av)
                 elif in_key in ['alternate']:
-                    out_args['link'] = out_args['unique_id'] = unicode(in_value)
+                    entry_out_args['link'] = entry_out_args['unique_id'] = unicode(in_value)
                 elif in_key in ['content']:
-                    out_args['description'] = html2text(in_value)
-                    out_args['content_encoded'] = unicode(in_value)
+                    entry_out_args['description'] = html2text(in_value)
+                    entry_out_args['content_encoded'] = unicode(in_value)
                 elif in_key in ['published']:
-                    out_args['pubdate'] = dateparse.parse_datetime(in_value)
+                    entry_out_args['pubdate'] = dateparse.parse_datetime(in_value)
                 elif in_key in ['updated']:
-                    out_args['updateddate'] = dateparse.parse_datetime(in_value)
+                    entry_out_args['updateddate'] = dateparse.parse_datetime(in_value)
                 elif in_key in ['categories', 'comments', 'id']:
                     pass
                 elif in_key in ['title']:
-                    out_args[in_key] = html2text(in_value)
+                    entry_out_args[in_key] = html2text(in_value)
                 else:
-                    out_args[in_key] = unicode(in_value)
-            feed_out.add_item(**out_args)
-                
+                    entry_out_args[in_key] = unicode(in_value)
+            feed_out.add_item(**entry_out_args)
+
         feed_out.write(self.response, 'utf-8')
 
 #########################################################################################
